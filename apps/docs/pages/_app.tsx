795e95fc31542565884b5398d8a82c474532267d
Alistair Smith <hi@alistair.sh>
2022-09-14 02:49:44 +0100

    feat: update documentation

---
+++ apps/docs/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../styles/globals.css';
+
+import {AppProps} from 'next/app';
+import React from 'react';
+
+export default function App({Component, pageProps}: AppProps) {
+	return <Component {...pageProps} />;
+}
