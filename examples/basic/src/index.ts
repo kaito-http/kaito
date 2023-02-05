52660d6d49ec58f5f52379dfef5c0c2d36e2d0f5
Alistair Smith <hi@alistair.sh>
2023-02-05 05:56:36 +0000

    feat: begin working on kaito 3.0

--- examples/basic/src/index.ts
+++ examples/basic/src/index.ts
@@ -1,140 +1,36 @@
-import {createServer, KaitoError} from '@kaito-http/core';
+import {kaito, createContext} from '@kaito-http/core';
 import {z} from 'zod';
-import {createRouter, getContext} from './context';
 
-const users = createRouter().add('POST', '/:id', {
-	body: z.object({
-		name: z.string(),
-	}),
-
-	query: {
-		limit: z
-			.string()
-			.transform(value => parseInt(value, 10))
-			.default('10'),
-	},
-
-	async run({ctx, body, params, query}) {
-		return {
-			uptime: ctx.uptime,
-			body,
-			params,
-			query,
-		};
-	},
+const {getContext, router} = createContext(async () => {
+	return {
+		foo: 'bar',
+	};
 });
 
-const v1 = createRouter()
-	// Basic inline route
-	.add('GET', '/time', async () => Date.now())
-
-	// Basic object route
-	.add('POST', '/time', {
-		async run() {
-			return Date.now();
+const app = router().get(
+	'/users/:discord_id/kv/:key',
+	{
+		// body: z.object({
+		// 	name: z.string(),
+		// }),
+		response: {
+			200: z.string(),
+			401: z.literal('You are not authorized'),
 		},
-	})
-
-	// How to throw an error
-	.add('GET', '/throw', async () => {
-		throw new KaitoError(400, 'Something was intentionally thrown');
-	})
-
-	// Example parsing request body
-	.add('POST', '/echo', {
-		body: z.record(z.string(), z.unknown()),
-
-		async run({body}) {
-			// Body is typed as `Record<string, unknown>`
-			return body;
-		},
-	})
-
-	// Merge this router with another router (users).
-	.merge('/users', users);
-
-const router = createRouter()
-	// Basic inline access context
-	.add('GET', '/uptime', async ({ctx}) => ctx.uptime)
-	.add('POST', '/uptime', async ({ctx}) => ctx.uptime)
-
-	// Accessing query
-	.add('GET', '/query', {
-		query: {
-			age: z
-				.string()
-				.default('10')
-				.transform(value => parseInt(value, 10)),
-		},
-
-		run: async ({query}) => query.age,
-	})
-
-	// Merge this router with another router (v1)
-	.merge('/v1', v1);
-
-const server = createServer({
-	router,
-	getContext,
-
-	// Example of handling routes outside of Kaito.
-	// Raw routes are not typed, and you have to handle errors yourself.
-	// They are really a last resort! The use case is, for example, handling
-	// stripe webhooks, OAuth, etc. Things like that.
-	rawRoutes: {
-		GET: [
-			{
-				path: '/',
-				handler(request, response) {
-					// You can access body by using something like `raw-body` on NPM.
-					// This is not included in Kaito's raw routes because raw routes
-					// are just a wrapper around Node's `http` module.
-
-					response.end("welcome to kaito's raw routes");
-				},
-			},
-		],
 	},
+	d => {
+		d.body;
 
-	// Before runs code before every request. This is helpful for setting things like CORS.
-	// You can return a value from before, and it will be passed to the after call.
-	// If you end the response in `before`, the router will not be called.
-	async before(req, res) {
-		res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-		res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-		res.setHeader('Access-Control-Max-Age', '86400');
-		res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-		if (req.method === 'OPTIONS') {
-			res.statusCode = 204;
-			// This is safe, because the router will know that the response is ended.
-			res.end();
+		if (Math.random() > 0.8) {
+			return null;
 		}
 
-		// Return something from `before`, and it will be passed to `after`.
-		return {
-			timestamp: Date.now(),
-		};
-	},
+		return null;
+	}
+);
 
-	// Access the return value from `before` in `after`.
-	// If the `before` function above ends the response, after will *still* be called!
-	// So be careful about logging request durations etc
-	async after({timestamp}) {
-		console.log(`Request took ${Date.now() - timestamp}ms`);
-	},
-
-	async onError({error}) {
-		console.log(error);
-
-		return {
-			status: 500,
-			message: error.message,
-		};
-	},
+const server = kaito(app, {
+	getContext,
 });
 
-server.listen(8080);
-
-export type App = typeof router;
+void server.listen(8080);
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,23 +1,181 @@
+import {z} from 'zod';
+
 export interface KaitoServer {
-	//
+	listen: (port: number) => Promise<void>;
+	attachHandler: (handler: KaitoHandler) => void;
 }
+
+export type ExtractRouteParams<T extends string> = string extends T
+	? string
+	: T extends `${string}:${infer Param}/${infer Rest}`
+	? Param | keyof ExtractRouteParams<Rest>
+	: T extends `${string}:${infer Param}`
+	? Param
+	: never;
+
+export type KaitoMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'HEAD' | 'OPTIONS' | 'CONNECT' | 'TRACE';
 
 export interface KaitoRequest {
 	//
 }
 
-export const servers = {
-	node: async () => {
-		const http = await import('node:http');
-
-		const server = await http.createServer();
-	},
-};
-
-export interface KaitoOptions {
+export interface KaitoResponse {
 	//
 }
 
-export function kaito(options: KaitoOptions) {
-	//
+export type KaitoHandler = (request: KaitoRequest) => Promise<KaitoResponse>;
+
+export const servers = {
+	node: async (): Promise<KaitoServer> => {
+		const http = await import('node:http');
+
+		const server = http.createServer();
+
+		return {
+			listen: async port => {
+				return new Promise(resolve => {
+					server.listen(port, resolve);
+				});
+			},
+
+			attachHandler: handler => {
+				//
+			},
+		};
+	},
+};
+
+export interface KaitoOptions<Context> {
+	// TODO: This is not final
+	getContext: () => Promise<Context>;
+	server?: KaitoServer | (() => Promise<KaitoServer> | KaitoServer);
+}
+
+export type KaitoRouteSchema<
+	M extends KaitoMethod,
+	BodyOut,
+	BodyDef extends z.ZodTypeDef,
+	BodyIn,
+	Output extends Record<number, z.ZodTypeAny>
+> = {
+	description?: string;
+	tags?: string[];
+	response: Output;
+	body?: M extends 'GET' ? never : z.Schema<BodyOut, BodyDef, BodyIn>;
+};
+
+export type KaitoRouteDefinition<
+	Context,
+	M extends KaitoMethod,
+	Path extends string,
+	BodyOut,
+	BodyDef extends z.ZodTypeDef,
+	BodyIn,
+	Output extends Record<number, z.ZodTypeAny>
+> = {
+	method: M;
+	path: Path;
+	schema: KaitoRouteSchema<M, BodyOut, BodyDef, BodyIn, Output>;
+	handler: KaitoRouteHandler<Context, M, Path, Body, Output>;
+};
+
+export type AnyKaitoRouteDefinition<Context> = KaitoRouteDefinition<
+	Context,
+	any,
+	any,
+	any,
+	z.ZodTypeDef,
+	any,
+	Record<number, z.ZodTypeAny>
+>;
+
+export type KaitoRouteCreator<
+	Context,
+	ExistingRoutes extends AnyKaitoRouteDefinition<Context>[],
+	M extends KaitoMethod
+> = <
+	Path extends string,
+	Output extends Record<number, z.ZodTypeAny>,
+	BodyDef extends z.ZodTypeDef,
+	BodyIn,
+	BodyOut = undefined
+>(
+	path: Path,
+	spec: KaitoRouteSchema<M, BodyOut, BodyDef, BodyIn, Output>,
+	handler: KaitoRouteHandler<Context, M, Path, BodyOut, Output>
+) => KaitoRouter<
+	Context,
+	[
+		...ExistingRoutes,
+		Extract<KaitoRouteDefinition<Context, M, Path, BodyOut, BodyDef, BodyIn, Output>, AnyKaitoRouteDefinition<Context>>
+	]
+>;
+
+export type KaitoHandlerArgument<Context, Path extends string, Body> = {
+	ctx: Context;
+	params: Record<ExtractRouteParams<Path>, string>;
+	body: Body;
+};
+
+export type KaitoRouteHandler<
+	Context,
+	Method extends KaitoMethod,
+	Path extends string,
+	BodyOut,
+	Output extends Record<number, z.ZodTypeAny>
+> = (arg: KaitoHandlerArgument<Context, Path, BodyOut>) => null;
+
+export interface KaitoRouter<Context, Routes extends AnyKaitoRouteDefinition<Context>[]> {
+	routes: Routes;
+	get: KaitoRouteCreator<Context, Routes, 'GET'>;
+	post: KaitoRouteCreator<Context, Routes, 'POST'>;
+	put: KaitoRouteCreator<Context, Routes, 'PUT'>;
+	patch: KaitoRouteCreator<Context, Routes, 'PATCH'>;
+	delete: KaitoRouteCreator<Context, Routes, 'DELETE'>;
+	head: KaitoRouteCreator<Context, Routes, 'HEAD'>;
+	options: KaitoRouteCreator<Context, Routes, 'OPTIONS'>;
+	connect: KaitoRouteCreator<Context, Routes, 'CONNECT'>;
+	trace: KaitoRouteCreator<Context, Routes, 'TRACE'>;
+}
+
+export function createContext<T>(getContext: () => Promise<T>) {
+	return {
+		getContext,
+
+		router: (): KaitoRouter<T, []> => {
+			return {
+				routes: [],
+				get: (path, spec, handler) => null as any,
+				post: (path, spec, handler) => null as any,
+				put: (path, spec, handler) => null as any,
+				patch: (path, spec, handler) => null as any,
+				delete: (path, spec, handler) => null as any,
+				head: (path, spec, handler) => null as any,
+				options: (path, spec, handler) => null as any,
+				connect: (path, spec, handler) => null as any,
+				trace: (path, spec, handler) => null as any,
+			};
+		},
+	};
+}
+
+export function kaito<Context>(
+	router: KaitoRouter<Context, AnyKaitoRouteDefinition<Context>[]>,
+	options: KaitoOptions<Context>
+) {
+	return {
+		listen: async (port: number) => {
+			const server = await Promise.resolve(
+				options?.server === undefined
+					? servers.node()
+					: options.server instanceof Function
+					? options.server()
+					: options.server
+			);
+
+			await server.listen(port);
+
+			return server;
+		},
+	};
 }
