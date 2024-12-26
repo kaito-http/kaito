d58b62f9490983ff90f425a75d85f5fe17991c57
Cody Miller <hello@looskie.com>
2024-12-26 12:06:19 -0500

    docs

--- apps/docs/src/pages/recipes/_meta.ts
+++ apps/docs/src/pages/recipes/_meta.ts
@@ -3,6 +3,7 @@ import type {Meta} from 'nextra';
 const meta: Meta = {
 	'getting-clients-ip': "Getting the Client's IP",
 	cors: 'Handling CORS',
+	cookies: 'Handling Cookies',
 };
 
 export default meta;
