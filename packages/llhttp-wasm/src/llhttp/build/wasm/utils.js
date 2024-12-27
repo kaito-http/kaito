d83221258821a1f4597d44a2b30e5da992efc2ed
TheLDB <ldb@erikboles.com>
2024-12-26 23:54:26 -0700

    prettier write

--- packages/llhttp-wasm/src/llhttp/build/wasm/utils.js
+++ packages/llhttp-wasm/src/llhttp/build/wasm/utils.js
@@ -1,14 +1,22 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', {value: true});
 exports.enumToMap = enumToMap;
 function enumToMap(obj, filter = [], exceptions = []) {
-    var _a, _b;
-    const emptyFilter = ((_a = filter === null || filter === void 0 ? void 0 : filter.length) !== null && _a !== void 0 ? _a : 0) === 0;
-    const emptyExceptions = ((_b = exceptions === null || exceptions === void 0 ? void 0 : exceptions.length) !== null && _b !== void 0 ? _b : 0) === 0;
-    return Object.fromEntries(Object.entries(obj).filter(([, value]) => {
-        return (typeof value === 'number' &&
-            (emptyFilter || filter.includes(value)) &&
-            (emptyExceptions || !exceptions.includes(value)));
-    }));
+	var _a, _b;
+	const emptyFilter =
+		((_a = filter === null || filter === void 0 ? void 0 : filter.length) !== null && _a !== void 0 ? _a : 0) === 0;
+	const emptyExceptions =
+		((_b = exceptions === null || exceptions === void 0 ? void 0 : exceptions.length) !== null && _b !== void 0
+			? _b
+			: 0) === 0;
+	return Object.fromEntries(
+		Object.entries(obj).filter(([, value]) => {
+			return (
+				typeof value === 'number' &&
+				(emptyFilter || filter.includes(value)) &&
+				(emptyExceptions || !exceptions.includes(value))
+			);
+		}),
+	);
 }
 //# sourceMappingURL=utils.js.map
