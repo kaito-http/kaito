ecb1742a5686e55bb7a537081ddbc300aec1285c
Alistair Smith <hi@alistair.sh>
2024-12-29 17:36:31 +0000

    server

--- packages/node/modules/server/server.v
+++ packages/node/modules/server/server.v
@@ -4,47 +4,9 @@ import json
 import picoev
 import picohttpparser
 
-struct Message {
-	message string
-}
-
-@[inline]
-fn json_response() string {
-	msg := Message{
-		message: 'Hello, World!'
-	}
-	return json.encode(msg)
-}
-
-@[inline]
-fn hello_response() string {
-	return 'Hello, World!'
-}
-
 fn callback(data voidptr, req picohttpparser.Request, mut res picohttpparser.Response) {
-	if req.method == 'GET' {
-		if req.path == '/t' {
-			res.http_ok()
-			res.header_server()
-			res.header_date()
-			res.plain()
-			res.body(hello_response())
-		} else if req.path == '/j' {
-			res.http_ok()
-			res.header_server()
-			res.header_date()
-			res.json()
-			res.body(json_response())
-		} else {
-			res.http_ok()
-			res.header_server()
-			res.header_date()
-			res.html()
-			res.body('Hello Picoev!\n')
-		}
-	} else {
-		res.http_405()
-	}
+	res.http_ok()
+	res.body(lol)
 	res.end()
 }
 
