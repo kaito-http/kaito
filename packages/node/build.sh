c7cab5f003003007cce8e777be3d360c3fb18c06
Alistair Smith <hi@alistair.sh>
2024-12-29 18:58:06 +0000

    some stuff

--- packages/node/build.sh
+++ packages/node/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# v -d no_backtrace -shared binding.v -o binding.node -autofree
 v -d no_backtrace -shared binding.v -o binding.node -cg
+
 mv binding.node.dylib binding.node
--- packages/node/modules/server/server.v
+++ packages/node/modules/server/server.v
@@ -4,6 +4,20 @@ import picoev
 import picohttpparser
 
 fn callback(data voidptr, req picohttpparser.Request, mut res picohttpparser.Response) {
+	if req.method != 'GET' {
+		mut body_reader := req.get_body_reader(req.fd) or { panic('Failed to get body reader') }
+
+		body := body_reader.read_all() or {
+			res.status(400)
+			res.body('Failed to read body')
+			res.end()
+			return
+		}
+
+		body_as_utf8 := body.bytestr()
+		println(body_as_utf8)
+	}
+
 	res.status(200)
 	res.body('hi')
 	res.end()
