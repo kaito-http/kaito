5d9efdb3efcd8c88bfe28a0f0ce77c8479255f12
Alistair Smith <hi@alistair.sh>
2025-02-05 16:56:05 +0000

    big router

--- packages/core/test/big-router/bigger-router.ts
+++ packages/core/test/big-router/bigger-router.ts
@@ -1,7 +1,17 @@
 import {bigrouter} from './big-router.ts';
 import {router} from './router.ts';
 
-export const biggerRouter = router().merge('/bigrouter', bigrouter);
+export const biggerRouter = router()
+	.merge('/bigrouter', bigrouter)
+	.merge('/bigrouter2', bigrouter)
+	.merge('/bigrouter3', bigrouter)
+	.merge('/bigrouter4', bigrouter)
+	.merge('/bigrouter5', bigrouter)
+	.merge('/bigrouter6', bigrouter)
+	.merge('/bigrouter7', bigrouter)
+	.merge('/bigrouter8', bigrouter)
+	.merge('/bigrouter9', bigrouter)
+	.merge('/bigrouter10', bigrouter);
 
 console.log(biggerRouter.routes.size);
 
