b70ac90ae9b15fc81fef1b2208a038a87de583f0
Alistair Smith <hi@alistair.sh>
2025-02-08 20:45:44 +0000

    change export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,5 +4,5 @@ export * from './head.ts';
 export * from './request.ts';
 export * from './route.ts';
 export * from './router/router.ts';
-export type {KaitoMethod} from './router/types.ts';
+export * from './router/types.ts';
 export * from './util.ts';
