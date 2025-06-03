8d23f578a9e7825472b3095cc32ced4bdc8c54b0
Alistair Smith <hi@alistair.sh>
2025-06-03 11:55:15 -0700

    update docs

--- examples/ai/src/context.ts
+++ examples/ai/src/context.ts
@@ -1,13 +1,8 @@
 import {createUtilities} from '@kaito-http/core';
-import {getRemoteAddress} from '@kaito-http/uws';
 
 const serverStarted = Date.now();
 
 export const {getContext, router} = createUtilities(async (req, _res) => {
-	// Use `getRemoteAddress()` in Kaito 3.0.0+ in Node.js. Can only be called
-	// inside this getContext function, or inside of a route (or the callstack of a route)
-	const ip = getRemoteAddress();
-
 	// Passing req is OK, but I personally prefer to avoid it.
 	// Instead, the logic I would have used req for should be
 	// included in this context file, allowing for it to be
@@ -15,7 +10,6 @@ export const {getContext, router} = createUtilities(async (req, _res) => {
 
 	return {
 		req,
-		ip,
 		uptime: Date.now() - serverStarted,
 	};
 });
--- examples/basic/src/context.ts
+++ examples/basic/src/context.ts
@@ -1,14 +1,9 @@
 import {createUtilities} from '@kaito-http/core';
-import {getRemoteAddress} from '@kaito-http/uws';
 import {serialize, type SerializeOptions} from 'cookie';
 
 const serverStarted = Date.now();
 
 export const {getContext, router} = createUtilities(async (req, res) => {
-	// Use `getRemoteAddress()` in Kaito 3.0.0+ in Node.js. Can only be called
-	// inside this getContext function, or inside of a route (or the callstack of a route)
-	const ip = getRemoteAddress();
-
 	// Passing req is OK, but I personally prefer to avoid it.
 	// Instead, the logic I would have used req for should be
 	// included in this context file, allowing for it to be
@@ -16,7 +11,6 @@ export const {getContext, router} = createUtilities(async (req, res) => {
 
 	return {
 		req,
-		ip,
 
 		cookie(name: string, value: string, options: SerializeOptions = {}) {
 			res.headers.set('Set-Cookie', serialize(name, value, options));
