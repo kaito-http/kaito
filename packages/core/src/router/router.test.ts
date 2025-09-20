a41cca1428b608f78971cdfd2f4d6610eb5b34d4
Alistair Smith <hi@alistair.sh>
2025-09-20 16:39:30 -0700

    test

--- packages/core/src/router/router.test.ts
+++ packages/core/src/router/router.test.ts
@@ -1,12 +1,14 @@
 import assert from 'node:assert';
 import {describe, it} from 'node:test';
 import {KaitoError} from '../error.ts';
+import type {KaitoRequest} from '../request.ts';
 import type {AnyRoute} from '../route.ts';
 import {k} from '../schema/schema.ts';
 import type {KaitoMethod} from '../util.ts';
 import {Router} from './router.ts';
 
-const router = Router.create({
+type Tc = {req: KaitoRequest};
+const router = Router.create<Tc>({
 	getContext: req => ({req}),
 	onError: () => ({status: 500, message: 'Internal Server Error'}),
 });
