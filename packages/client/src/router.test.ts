0fb45020bfb9366a4202bb196b687a9f16d49461
Alistair Smith <hi@alistair.sh>
2026-03-08 17:25:22 -0700

    fix route type arity in tests

--- packages/client/src/router.test.ts
+++ packages/client/src/router.test.ts
@@ -7,9 +7,9 @@ export type App = Router<
 	Ctx,
 	Ctx,
 	never,
-	| Route<Ctx, Ctx, [], {id: number; name: string}[], '/users', never, 'GET', {limit: string}, never>
-	| Route<Ctx, Ctx, [], {id: number; name: string}, '/users', never, 'POST', {}, {name: string}>
-	| Route<Ctx, Ctx, [], {id: number; name: string}, '/users/:id', never, 'GET', {}, never>
-	| Route<Ctx, Ctx, [], KaitoSSEResponse<SSEEvent<unknown, string>>, '/stream', never, 'GET', {}, never>,
+	| Route<Ctx, Ctx, [], {id: number; name: string}[], '/users', never, 'GET', {limit: string}, never, never>
+	| Route<Ctx, Ctx, [], {id: number; name: string}, '/users', never, 'POST', {}, {name: string}, {name: string}>
+	| Route<Ctx, Ctx, [], {id: number; name: string}, '/users/:id', never, 'GET', {}, never, never>
+	| Route<Ctx, Ctx, [], KaitoSSEResponse<SSEEvent<unknown, string>>, '/stream', never, 'GET', {}, never, never>,
 	[]
 >;
